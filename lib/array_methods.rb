5709590c194d52e319b0b9a16341e43e083cd6ef
Heidi Boulter <boulter.heidi@gmail.com>
2019-10-11 03:24:38 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,22 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  counter = 0
+
+  while counter < array.length do
+  array.include?(value_to_find)
+  counter += 1
+end
+  p array.index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  counter = 0
+
+  while counter < array.length do
+    array.max(1)
+    counter += 1
+  end
 end
 
 def find_min_value(array)
-  # Add your solution here
+
 end
